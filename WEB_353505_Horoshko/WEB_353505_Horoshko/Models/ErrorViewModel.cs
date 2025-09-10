52fd53a3c3f3bb8f6de6bb11fef51d00e8703f27
Kirill <khorosko15@gmail.com>
2025-09-11 01:15:38 +0300

    Init project
    Create project WEB_353505_Horoshko

---
+++ WEB_353505_Horoshko/WEB_353505_Horoshko/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WEB_353505_Horoshko.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
