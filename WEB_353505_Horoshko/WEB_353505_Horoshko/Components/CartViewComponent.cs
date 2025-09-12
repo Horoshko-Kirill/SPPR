a2eeced963804aa355b70bea2f29d04a790842f4
Kirill <khorosko15@gmail.com>
2025-09-12 03:38:12 +0300

    Add 1 and 2 labs

---
+++ WEB_353505_Horoshko/WEB_353505_Horoshko/Components/CartViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_353505_Horoshko.Components
+{
+    public class CartViewComponent : ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ WEB_353505_Horoshko/WEB_353505_Horoshko/Models/ViewsModel/ListDemoViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WEB_353505_Horoshko.Models.ViewsModel
+{
+    public class ListDemoViewModel
+    {
+        public SelectList Items { get; set; }
+    }
+}
